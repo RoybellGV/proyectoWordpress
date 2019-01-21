085fd6edb83216fcd099478b6cf1279d01571bcd
RoybellGV <43592722+RoybellGV@users.noreply.github.com>
2019-01-21 14:20:27 -0600

    Add files via upload

---
+++ page-acerca.php
@@ -0,0 +1,134 @@
+<html>
+    
+ <?php get_header();?>
+ 
+    <div class="container-fluid">
+    
+    <!-- Seccion Nosotros -->
+        
+              <div class="container-fluid bannerFondo mb-5">
+
+                <div class="row aboutBanner align-items-center">
+                  <div class="col text-center">
+                    <h1 class="display-1"> <strong>Nosotros</strong></h1>
+                      <h3 >Enfocados en el servicio de las más alta calidad para clientes con un alto paladar refinado</h3>  
+                  </div>
+                </div>
+
+              </div>
+  <!-- Fin Seccion nosotros-->
+          <hr/>
+<!-- Seccion Quienes somos-->
+        <div class="container mt-3">
+          <h1 class="display-1 text-center"> <strong>¿Quienes somos?</strong></h1>
+          <div class="row align-items-center">
+  
+                  <div class="col-md-5 col-sm-12 pr-0"><img class="img-fluid" src="css/img/imgAbout1.jpg" alt=""></div>
+  
+                  <div class="col-md-7 col-sm-12 bg-warning seccionQuiSom">
+                    <h3 class="font-weight-bold pl-5 mt-3 text-white">Nuestro Compromiso</h3>
+                     <p class="pl-5">Llevar platillos de calidad a su es y que con cada bocado tenga la mejor experiencia.</p>
+                    <h3 class="font-weight-bold pl-5 text-white">Porque Escogernos</h3>
+                    <p class="pl-5">Con más de 20 años  de experiencia liderando restaurantes cinco estrellas.</p>
+                    <p class="pl-5">Ganadores de estrellas Michelin en los ultimos años</p>
+                </div>
+         
+        </div>
+         </div>
+
+  
+    <!-- Fin Seccion Quienes somos-->
+<hr/>
+
+
+ <!-- Seccion Historia-->
+    <div class="container-fluid imgHistoria aboutBanner2 mb-5">
+
+                  <div class="row align-items-center">
+                    <div class="col text-center">
+                      <h1 class="display-1 mt-4"> <strong>Historia</strong></h1>
+
+                    </div>
+                  </div>
+
+                  <div class="row text-center HistoriaCaja d-flex justify-content-center">
+                    <div class="col-md-12 d-flex justify-content-center mt-4">
+                        <div class="col-md-3 degradado mr-2 ml-5">
+                            <h3 class="mt-4">Establecimiento</h3>
+                            <p>Un restaurante con un toque rustico.</p>
+                         </div>
+
+                         <div class="col-md-3 degradado mx-2">
+                            <h3 class="mt-4">Exitos</h3>
+                            <p>Ganadores de estrellas Michelin</p>
+                         </div>
+
+                         <div class="col-md-3  degradado ml-2 mr-5">
+                            <h3 class="mt-4">Tecnologias</h3>
+                            <p>Las más innovadoras y vanguardistas de la epoca</p>
+                         </div>
+                      </div>
+                  </div>
+          </div>
+    <!-- Fin Seccion Historia-->
+    <hr/>
+
+
+    <!-- Seccion Testimonio-->
+
+      <div class="container-fluid mb-5">
+                    <div class="row">
+                      <div class="col text-center">
+                         <h1 class="display-1 mt-4"> <strong>Testimonio</strong></h1>
+
+                      </div>
+                    </div>
+
+                  <div class="row d-flex justify-content-center">
+                    
+                    <div class="col-md-6 mb-4">
+                    <!--Card-->
+                    <div class="card testimonial-card text-center bordeCard">
+                        <!--Bacground color-->
+                        <div class="card-up testimonio">
+                        </div>
+                        <!--Avatar-->
+                        <div class="avatar mx-auto white">
+                            <img src="css/img/imgAbout3.jpg" width="150" height="150" class="rounded-circle imgTestimonio">
+                        </div>
+                        <div class="card-body">
+                            <!--Name-->
+                            <h4 class="card-title text-uppercase">Anna Doe</h4>
+                            <h5 class="text-danger">Cliente</h5>
+                            <hr>
+                            <!--Quotation-->
+                            <p>
+                                <i class="fas fa-quote-left"></i> Super complacida con el servicio recibido, platillos deliciosos.</p>
+
+                        </div>
+                    </div>
+                    <!--/.Card-->
+                  </div>
+                </div>
+            </div>
+
+
+    <!-- Fin Seccion Testimonio-->
+     <hr/>
+
+    <!-- Seccion Mapa-->
+     <div class="container-fluid d-flex justify-content-center mt-5">
+          <div class="col-md-10 mb-5 ">
+                <div  class="map bordeCard">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.717802408508!2d-84.84214851303754!3d9.978416510070142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa02ec06f8ff217%3A0x2b91ac63c996104b!2sCapitan+Moreno!5e0!3m2!1ses-419!2scr!4v1548031228622" allowfullscreen></iframe>
+                </div>
+         
+        </div>
+</div>
+    <!-- Fin Seccion Mapa-->
+    </div>
+<!-- Section: Contact v.3 -->
+
+  <?php get_footer();?>
+
+</html>
