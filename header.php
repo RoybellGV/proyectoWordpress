4d06051f93dbc2fda93eaf3e6d555d14eb6dcaa2
RoybellGV <43592722+RoybellGV@users.noreply.github.com>
2019-01-21 13:41:34 -0600

    Add files via upload
    Este commit arregla el nav

--- header.php
+++ header.php
@@ -12,48 +12,25 @@
         
         
 <!--Navbar -->
-<nav class="mb-1 icono navbar navbar-expand-lg navbar-dark orange contrast lighten-1">
-     <ul class="navbar-nav ml-auto nav-flex-icons">
-      <li class="nav-item avatar">
-        <a class="nav-link p-0" href="#">
-            <img src="https://i.ibb.co/Y4CT1FT/icono.png" alt="icono" border="0" class="rounded-circle z-depth-0" alt="avatar image" height="45">
-         
-        </a>
-      </li>
-    </ul>
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
-    aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Menu</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Gallery</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Team</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="page-contact.php">Contact</a>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Blog</a>
-      </li>
-    </ul>
-   
-  </div>
-
+<nav class="navbar navbar-expand-md navbar-light bg-light " role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+	<button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="#">Navbar</a>
+        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'primary',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker()
+		) );
+        ?>
+    </div>
 </nav>
 <!--/.Navbar -->
