8853a61519162352bfdac11b60db4a37398698b1
RoybellGV <43592722+RoybellGV@users.noreply.github.com>
2019-01-30 08:30:39 -0600

    Add files via upload
    404

---
+++ 404.php
@@ -0,0 +1,28 @@
+<html>
+   <head>
+	<title><?php bloginfo('name');?></title>
+    <meta name="viewport" content="width=device-width, initial-scale-1, maximum-scale-1, user,scalable=no">
+    <meta charset="<?php bloginfo('charset');?>">
+	<?php wp_head();?>
+    
+    
+    
+</head>
+<div class="container-fluid">
+    
+<div class="container-fluid  mb-5 col-md-12 col-lg-12 text-center">
+    <img class="img-fluid" src="https://i.ibb.co/DRtszLz/error.png" alt="error"  width="520vh">
+</div>
+    
+<div class="text-dark">
+        <h1 class="mt-5">¡Página no encontrada!</h1>
+        <h2>No pudimos encontrar la página que buscas</h2>
+        <p id="descripcion"><button class="color bottonsize mt-3" href >Visitar Home</button><button class="color bottonsize" href >Contáctenos</button></p>
+   
+</div>
+
+
+
+
+</div>
+</html>
