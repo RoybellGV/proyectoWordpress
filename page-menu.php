9c7ca54e4fc93e931cacfee1192076d36f1f579d
Alvarez1515 <44753424+Alvarez1515@users.noreply.github.com>
2019-02-10 21:42:53 -0600

    Add files via upload
    estilo y menu.... el estilo fue hecho antes de pasarme el estilo con la barra negra del home

---
+++ page-menu.php
@@ -0,0 +1,349 @@
+<html>
+
+<?php get_header();?>
+  <div class="col-md-12 col-lg-12">
+        
+         <!-- Seccion Platos deliciosos -->
+           <div class="container-fluid py-5 mt-5">
+
+                <div class="row  d-flex justify-content-center">
+                  <div class="col-md-10 text-center ">
+                   
+                     <h1 class="tipografia1 h1-responsive"> <strong>Platos deliciosos</strong></h1>
+                     <blockquote class="blockquote">
+                           <h3 class="tipografia2 h5-responsive mx-3">Contamos con gran variedad de platillos hechos por chef de varios paises, que dan su toque personal a cada platillo, dando un gusto unico.</h3>
+                        </blockquote>   
+                  </div>
+                </div>
+              </div>
+      <!-- Fin Seccion Platos deliciosos-->
+
+
+
+      <!-- Fin Seccion  equipo-->
+      
+      <!-- Galleria menu -->
+      <div class="row  d-flex justify-content-center">
+<div class="classic-tabs col-md-10">
+
+  <ul class="nav bg-dark d-flex justify-content-center mb-5" id="myClassicTab" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link  waves-light active show text-uppercase" id="profile-tab-classic" data-toggle="tab" href="#profile-classic" role="tab" aria-controls="profile-classic" aria-selected="true">Postres</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link waves-light text-uppercase" id="follow-tab-classic" data-toggle="tab" href="#follow-classic" role="tab" aria-controls="follow-classic" aria-selected="false">Ensaladas</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link waves-light text-uppercase" id="contact-tab-classic" data-toggle="tab" href="#contact-classic" role="tab" aria-controls="contact-classic" aria-selected="false">Entrada</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link waves-light text-uppercase" id="awesome-tab-classic" data-toggle="tab" href="#awesome-classic" role="tab" aria-controls="awesome-classic" aria-selected="false">Plato fuerte</a>
+    </li>
+  </ul>
+  <div class="tab-content border-right border-bottom border-left rounded-bottom" id="myClassicTabContent">
+    <div class="tab-pane fade active show" id="profile-classic" role="tabpanel" aria-labelledby="profile-tab-classic">
+      
+
+
+ <div class="container-fluid">
+        
+  <div class="card-deck mt-5">
+  <div class="card">
+    <img src="https://i.ibb.co/gvS4LCn/imgMenu1.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Tres leches</strong></h4>
+            <!-- Subtitle -->
+          
+            <!-- Text -->
+            
+            <blockquote class="blockquote">
+                           <p class="card-text text-muted">Leche condensa, harina, azucar</p>
+            </blockquote> 
+
+          
+
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/fNRrL7z/imgMenu2.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Flan de coco</strong></h4>
+            <!-- Subtitle -->
+            
+            <!-- Text -->
+            
+            
+            <blockquote class="blockquote">
+                           <p class="card-text text-muted">Coco, leche condensa, evaporada</p>
+            </blockquote> 
+       
+
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/RTmMrXX/imgMenu3.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Arroz con leche</strong></h4>
+            <!-- Subtitle -->
+        
+            <!-- Text -->
+            
+            
+            <blockquote class="blockquote">
+                           <p class="card-text text-muted">Arroz, canela, azúcar</p>
+            </blockquote>
+
+
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/yXbGFW6/imgMenu4.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Cheesecake</strong></h4>
+            <!-- Subtitle -->
+
+            <!-- Text -->
+            
+            
+            <blockquote class="blockquote">
+                           <p class="card-text text-muted">Galleta Maria, queso crema</p>
+            </blockquote>
+
+        
+
+          </div>
+  </div>
+</div>
+
+<!-- Segunda Linea de chef -->
+  <div class="card-deck mt-5 mb-4">
+  <div class="card">
+    <img src="https://i.ibb.co/fDWdbZ0/imgMenu5.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Browny</strong></h4>
+            <!-- Subtitle -->
+        
+            <!-- Text -->
+            
+            
+             <blockquote class="blockquote">
+                           <p class="card-text text-muted">Chocolate, harina, huevo</p>
+            </blockquote>
+
+         
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/qF1zHyV/imgMenu6.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Tiramisu</strong></h4>
+            <!-- Subtitle -->
+          
+            <!-- Text -->
+            
+             <blockquote class="blockquote">
+                           <p class="card-text text-muted">Cacao en plovo, café, huevo</p>
+            </blockquote>
+
+            
+          
+
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/p48cQZ1/imgMenu7.jpg" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Muffin</strong></h4>
+            <!-- Subtitle -->
+          
+            <!-- Text -->
+           
+            
+            
+             <blockquote class="blockquote">
+                            <p class="card-text text-muted">Harina, huevo, jugo de naranja</p>
+            </blockquote>
+
+
+          </div>
+  </div>
+  <div class="card">
+    <img src="https://i.ibb.co/1f4nt8C/imgMenu8.png" class="card-img-top" alt="...">
+    <div class="card-body card-body-cascade text-center">
+
+            <!-- Title -->
+            <h4 class="card-title"><strong>Pie de limón</strong></h4>
+            <!-- Subtitle -->
+         
+            <!-- Text -->
+           
+            
+             <blockquote class="blockquote">
+                            <p class="card-text text-muted">Queso crema, limón, harina </p>
+            </blockquote>
+
+          
+          </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+    </div>
+    <div class="tab-pane fade" id="follow-classic" role="tabpanel" aria-labelledby="follow-tab-classic">
+      <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+    </div>
+    <div class="tab-pane fade" id="contact-classic" role="tabpanel" aria-labelledby="contact-tab-classic">
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. </p>
+    </div>
+    <div class="tab-pane fade" id="awesome-classic" role="tabpanel" aria-labelledby="awesome-tab-classic">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+
+</div>
+</div>
+<!-- fin galleria menu -->
+
+      
+     <br>
+    <br>
+    <hr align="center" noshade="noshade" size="5" width="90%" />
+
+      <!-- Seccion  platos menu-->
+
+      <div class="container-fluid mt-5">
+        <div class="row d-flex justify-content-center ">
+          <div class="col-md-10">
+           <div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-7 col-xl-7 pb-3">
+          <!--Excerpt-->
+          <blockquote class="blockquote">
+                           <h3 class="mb-4 tipografia1 h1-responsive"><strong class="colorTexto">Plato de entrada</strong></h3>
+          <p>Halleacas..........................................$7.09</p>
+          <p class="text-muted">Carne de cerdo, masa, aceitunas, papas</p>
+
+          <p>Waffles de pan de Yuca...................................................$4.99</p>
+          <p class="text-muted">Harina de yuca, queso, huevo, mantequilla</p>
+
+          <p>Sopa fria de tomate..............................................$5.99</p>
+          <p class="text-muted">Pimientos, pepino, aceite de oliva</p>
+            </blockquote>
+          
+          
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-5 col-xl-5 mb-5">
+          <!--Featured image-->
+          <div class="view overlay rounded z-depth-2">
+            <img src="https://i.ibb.co/jhnZKX6/imgMenu9.jpg" alt="" class="img-fluid">
+            <a>
+              <div class="mask waves-effect waves-light"></div>
+            </a>
+          </div>
+        </div>
+        <!--Grid column-->
+
+      </div>
+
+<div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-5 col-xl-5 pb-3">
+          <!--Featured image-->
+          <div class="view overlay rounded z-depth-2">
+            <img src="https://i.ibb.co/rkCDtwV/img-Menu10.jpg" alt="" class="img-fluid">
+            <a>
+              <div class="mask waves-effect waves-light"></div>
+            </a>
+          </div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-7 col-xl-7">
+          <!--Excerpt-->
+         
+          
+          
+          <blockquote class="blockquote">
+          <h3 class="mb-4 tipografia1 h1-responsive"><strong class="colorTexto">Plato fuerte</strong></h3>
+         
+          
+          <p>Pechuga de pollo a la crema.................................................$7.09</p>
+          <p class="text-muted">Pollo, ajo, mantequilla, sal, pimienta, leche</p>
+
+          <p>Salmón en salsa de cilantro..............................................$4.99</p>
+          <p class="text-muted">Salmón con piel, vino blanco, mantequila</p>
+
+          <p>Solimillo en sal de tres chiles..................................................$5.99</p>
+          <p class="text-muted">Solomillo, chile, salsa, pimienta</p>
+            </blockquote>
+          
+        </div>
+        <!--Grid column-->
+
+      </div>
+
+
+
+
+
+
+          </div>
+          
+        </div>
+        
+      </div>
+
+      <!-- Fin Seccion  platos menu-->
+    
+
+      <!--  Seccion  Descuentos-->
+
+      <div class="mb-5 mt-5 ">
+        <div class="card card-image bannerMenu">
+          <div class="text-white d-flex align-items-center menuBanner rgba-black-strong">
+           
+            <div id="columnaMenu" class="col-md-12 col-lg-6"></div>
+            <div class=" col-12 col-md-12 col-lg-6">            
+                <h3 class="tipografia1 text-uppercase"><strong class="colorTexto">Felices clientes</strong></h3>
+
+              <h2 class="text-white font-weight-bold tipografia2"> <strong>DESCUENTOS DEL 50%</strong></h2>
+              <h4 class="text-white tipografia2">Recomendado entre nuestros clientes por su excelencia <br>y buen servicio al cliente.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <!-- Fin Seccion  Descuentos-->
+      
+      </div>
+  
+
+
+<?php get_footer();?>
+
+
+</html>
