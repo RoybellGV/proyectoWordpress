d2e583c4db13e7b0b23c073f7e024c5c8fc29187
RoybellGV <43592722+RoybellGV@users.noreply.github.com>
2019-02-11 15:29:47 -0600

    Add files via upload
    acerca arreglado

--- page-acerca.php
+++ page-acerca.php
@@ -1,169 +1,161 @@
 <html>
-
-<?php get_header();?>
-
-<div class="col-md-12 col-lg-12">
-    <!-- Seccion Nosotros -->
     
-    <div class="bannerFondo ">
-        <div class="rgba-black-strong col-md-12 col-lg-12 ">
-            <div class="text-white ">
-                <div class="row aboutBanner align-items-center">
-                    <div class="col text-center text-white">
-                        <h1 class="tipografia1 h1-responsive"> <strong class="colorTexto">Nosotros</strong></h1>
-                        
-                        <blockquote class="blockquote">
-                           <h3 class="tipografia2 h5-responsive mx-3">Enfocados en el servicio de las más alta calidad para clientes con un alto paladar refinado</h3>
-                        </blockquote>
-                    </div>
+ <?php get_header();?>
+ 
+
+  <div> 
+    <!-- Seccion Nosotros -->        
+          <div class="bannerFondo">
+          <div class="rgba-black-strong col-md-12 col-lg-12 ">
+          <div class="text-white ">
+            <div class="row aboutBanner align-items-center">
+                  <div class="col text-center text-white">
+                    <h1 class="tipografia1 h1-responsive"> <strong class="colorTexto">Nosotros</strong></h1>
+                      <h3 class="tipografia2 h5-responsive mx-3">Enfocados en el servicio de las más alta calidad para clientes con un alto paladar refinado</h3>  
+                  </div>
                 </div>
-            </div>
+          </div>
         </div>
-    </div>
-    <!-- Fin Seccion nosotros-->
+ </div>
+  <!-- Fin Seccion nosotros-->
+          
+          <br>
+          <br>
+          <hr align="center" noshade="noshade" size="5" width="90%" />
 
-    <br>
-    <br>
-    <hr align="center" noshade="noshade" size="5" width="90%" />
-
-    <!-- Seccion Quienes somos-->
-    <div class="mt-3">
-        <h1 class="h1-responsive text-center tipografia1"> <strong class="text-dark">¿Quienes somos?</strong></h1>
-        <div class="row align-items-center mt-5">
-
-            <div class="col-md-5 col-sm-12 pr-0"><img class="img-fluid" src="https://i.ibb.co/2W0ZfdD/img-About1.jpg" alt=""></div>
-
-            <div class="col-md-7 col-sm-12 colorfondo seccionQuiSom ">
-               
-               <blockquote class="blockquote">
-                           <h3 class="font-weight-bold pl-5 mt-3 text-white text-left tipografia2">Nuestro Compromiso</h3>
-                <p class="pl-5 text-dark tipografia2">Llevar platillos de calidad a su mesa y que con cada bocado tenga la mejor experiencia.</p>
-                <h3 class="font-weight-bold pl-5 text-white text-left tipografia2">Porque Escogernos</h3>
-                <p class="pl-5 text-dark tipografia2">Con más de 20 años de experiencia liderando restaurantes cinco estrellas.</p>
-                <p class="pl-5 text-dark tipografia2">Ganadores de estrellas Michelin en los ultimos años.</p>
-                        </blockquote>
-                
-            </div>
+<!-- Seccion Quienes somos-->
+        <div class="container mt-3">
+          <h1 class="h1-responsive text-center tipografia1"> <strong class="text-dark">¿Quienes somos?</strong></h1>
+          <div class="row align-items-center mt-5">
+  
+                  <div class="col-md-5 col-sm-12 pr-0"><img class="img-fluid" src="https://i.ibb.co/2W0ZfdD/img-About1.jpg" alt=""></div>
+  
+                  <div class="col-md-7 col-sm-12 colorfondo seccionQuiSom ">
+                     <blockquote class="blockquote"> 
+                    <h3 class="font-weight-bold pl-5 mt-3 text-white text-left tipografia2">Nuestro Compromiso</h3>
+                     <p class="pl-5 text-dark tipografia2">Llevar platillos de calidad a su es y que con cada bocado tenga la mejor experiencia.</p>
+                    <h3 class="font-weight-bold pl-5 text-white text-left tipografia2">Porque Escogernos</h3>
+                    <p class="pl-5 text-dark tipografia2">Con más de 20 años  de experiencia liderando restaurantes cinco estrellas.</p>
+                    <p class="pl-5 text-dark tipografia2">Ganadores de estrellas Michelin en los ultimos años.</p>
+                    </blockquote>
+                </div>
+         
         </div>
-    </div>
+         </div>
+
+  
     <!-- Fin Seccion Quienes somos-->
-    <br>
-    <br>
-    <hr align="center" noshade="noshade" size="5" width="90%" />
+            <br>
+          <br>
+          <hr align="center" noshade="noshade" size="5" width="90%" />
 
-    <!-- Seccion Historia-->
-    <div class="imgHistoria aboutBanner2 mt-5">
+ <!-- Seccion Historia-->
+<div class="container-fluid imgHistoria aboutBanner2 mt-5">
 
-        <div class="row align-items-center">
-            <div class="col text-center">
-                <h1 class="h1-responsive mt-4 tipografia1 "> <strong class="text-light">Historia</strong></h1>
-            </div>
+    <div class="row align-items-center">
+        <div class="col text-center">
+            <h1 class="h1-responsive mt-4 tipografia1 textHistoria text-light"> <strong class="">Historia</strong></h1>
         </div>
-
-        <div class="row d-flex d-flex justify-content-center">
-            <div class="card-deck text-center mt-4">
-                <div class="card degradado border-0">
-
-                    <div class="card-body">
-                        <h4 class="card-title tipografia2 "><strong class="text-dark">Establecimiento</strong></h4>
-                        
-                       
-                        <blockquote class="blockquote">
-                           <p class="card-text tipografia2 text-dark ">Un restaurante con un toque rustico</p>
-                        </blockquote>
-                    </div>
-                </div>
-                <div class="card degradado border-0">
-
-                    <div class="card-body">
-                        <h4 class="card-title tipografia2 "><strong class="text-dark">Exitos</strong></h4>
-                        <blockquote class="blockquote">
-                           <p class="card-text tipografia2 text-dark ">Ganadores de estrellas Michelin</p>
-                        </blockquote>
-                        
-                    </div>
-                </div>
-                <div class="card degradado border-0">
-
-                    <div class="card-body">
-                        <h4 class="card-title tipografia2 "><strong class="text-dark">Tecnologias</strong></h4>
-                        <blockquote class="blockquote">
-                           <p class="card-text tipografia2 text-dark ">Las más innovadoras y vanguardistas de la epoca</p>
-                        </blockquote>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-
     </div>
 
+   <div class="row d-flex d-flex justify-content-center">
+    <div class="card-deck text-center mt-4">
+        <div class="card degradado border-0">
+
+            <div class="card-body">
+                <h4 class="card-title tipografia2 "><strong class="text-dark">Establecimiento</strong></h4>
+                <blockquote class="blockquote">
+                <p class="card-text tipografia2 text-dark ">Un restaurante con un toque rustico</p>
+                </blockquote>
+            </div>
+        </div>
+        <div class="card degradado border-0">
+
+            <div class="card-body">
+                <h4 class="card-title tipografia2 "><strong class="text-dark">Exitos</strong></h4>
+                <blockquote class="blockquote">
+                <p class="card-text tipografia2 text-dark ">Ganadores de estrellas Michelin</p>
+                 </blockquote>
+            </div>
+        </div>
+        <div class="card degradado border-0">
+
+            <div class="card-body">
+                <h4 class="card-title tipografia2 "><strong class="text-dark">Tecnologias</strong></h4>
+                <blockquote class="blockquote">
+                <p class="card-text tipografia2 text-dark ">Las más innovadoras y vanguardistas de la epoca</p>
+                    </blockquote>
+            </div>
+        </div>
+    </div>
+ </div>
+
+</div>
+   
     <!-- Fin Seccion Historia-->
-    <br>
-    <br>
-    <hr align="center" noshade="noshade" size="5" width="90%" />
+       <br>
+          <br>
+          <hr align="center" noshade="noshade" size="5" width="90%" />
 
 
     <!-- Seccion Testimonio-->
 
-    <div class="mb-5 ">
-        <div class="row">
-            <div class="col text-center">
-                <h1 class="h1-responsive mt-4 tipografia1"> <strong class="text-dark">Testimonio</strong></h1>
+      <div class="container-fluid mb-5 ">
+                    <div class="row">
+                      <div class="col text-center">
+                         <h1 class="h1-responsive mt-4 tipografia1"> <strong class="text-dark">Testimonio</strong></h1>
 
-            </div>
-        </div>
-
-        <div class="row d-flex justify-content-center">
-
-            <div class="col-md-6 mb-4">
-                <!--Card-->
-                <div class="card testimonial-card text-center bordeCard">
-                    <!--Bacground color-->
-                    <div class="card-up testimonio">
+                      </div>
                     </div>
-                    <!--Avatar-->
-                    <div class="avatar mx-auto white">
-                        <img src="https://i.ibb.co/VqYdNr6/img-About3.jpg" width="150" height="150" class="rounded-circle imgTestimonio">
-                    </div>
-                    <div class="card-body">
-                        <!--Name-->
-                        <h4 class="card-title text-uppercase text-dark tipografia2">Anna Doe</h4>
-                        
-                        <blockquote class="blockquote">
-                           <h5 class="text-danger tipografia2">Cliente</h5>
-                        <hr>
-                        <!--Quotation-->
-                        <p class="text-dark tipografia2">Super complacida con el servicio recibido, platillos deliciosos.</p>
-                        </blockquote>
-                        
 
+                  <div class="row d-flex justify-content-center">
+                    
+                    <div class="col-md-6 mb-4">
+                    <!--Card-->
+                    <div class="card testimonial-card text-center bordeCard">
+                        <!--Bacground color-->
+                        <div class="card-up testimonio">
+                        </div>
+                        <!--Avatar-->
+                        <div class="avatar mx-auto white">
+                            <img src="https://i.ibb.co/VqYdNr6/img-About3.jpg" width="150" height="150" class="rounded-circle imgTestimonio">
+                        </div>
+                        <div class="card-body">
+                            <!--Name-->
+                            <h4 class="card-title text-uppercase text-dark tipografia2">Anna Doe</h4>
+                            <h5 class="text-danger tipografia2">Cliente</h5>
+                            <hr>
+                            <!--Quotation-->
+                            <blockquote class="blockquote">
+                            <p class="text-dark tipografia2">Super complacida con el servicio recibido, platillos deliciosos.</p>
+                                </blockquote>
+
+                        </div>
                     </div>
+                    <!--/.Card-->
+                  </div>
                 </div>
-                <!--/.Card-->
             </div>
-        </div>
-    </div>
 
 
     <!-- Fin Seccion Testimonio-->
-    <br>
-    <br>
-    <hr align="center" noshade="noshade" size="5" width="90%" />
+       <br>
+          <br>
+          <hr align="center" noshade="noshade" size="5" width="90%" />
 
     <!-- Seccion Mapa-->
-    <div class="d-flex justify-content-center mt-5">
-        <div class="col-md-10 mb-5 ">
-            <div class="map bordeCard">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.717802408508!2d-84.84214851303754!3d9.978416510070142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa02ec06f8ff217%3A0x2b91ac63c996104b!2sCapitan+Moreno!5e0!3m2!1ses-419!2scr!4v1548031228622" allowfullscreen></iframe>
-            </div>
-
+     <div class="container-fluid d-flex justify-content-center mt-5">
+          <div class="col-md-10 mb-5 ">
+                <div  class="map bordeCard">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31435.717802408508!2d-84.84214851303754!3d9.978416510070142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa02ec06f8ff217%3A0x2b91ac63c996104b!2sCapitan+Moreno!5e0!3m2!1ses-419!2scr!4v1548031228622" allowfullscreen></iframe>
+                </div>
+         
         </div>
-    </div>
-    <!-- Fin Seccion Mapa-->
-
-    <!-- Section: Contact v.3 -->
 </div>
-<?php get_footer();?>
+    <!-- Fin Seccion Mapa-->
+   
+<!-- Section: Contact v.3 -->
+</div> 
+  <?php get_footer();?>
 
 </html>
