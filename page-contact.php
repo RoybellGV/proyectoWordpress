01b6574f67d627c9a287791989523d5213a60cb4
RoybellGV <43592722+RoybellGV@users.noreply.github.com>
2019-02-21 10:40:40 -0600

    Add files via upload
    contac y gallery en ingles/ final ya estan traducidas y con los efectos respectivos

---
+++ page-contact.php
@@ -0,0 +1,112 @@
+<?php get_header();?>
+
+<!-- Section: Contact-->
+<section class="contact-section my-5  ">
+    <!-- Form with header -->
+    <div class="card col-sm-12 col-lg-12 ">
+        <!-- Grid row -->
+        <div class="row  claseDelDiv">
+            <!-- Grid column -->
+            <div class="col-lg-8 animated bounceInLeft">
+                <div class="card-body form">
+                    <!-- Header -->
+                    <h2 class="mt-4 text-center font-weight-bold tipografia1" style="color:#d4aa00ff" ;><i class="fas fa-envelope pr-2 "></i>Write Us:</h2>
+
+                    <!-- Grid row -->
+                    <div class="row tipografia1 font-weight-bold">
+
+                        <!-- Grid column -->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="form-contact-name" class="form-control">
+                                <h3><label for="form-contact-name" class="text-white">Name</label></h3>
+                            </div>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="form-contact-email" class="form-control">
+                                <h3><label for="form-contact-email" class="text-white">Email</label></h3>
+                            </div>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+
+                    <!-- Grid row -->
+                    <div class="row tipografia1 font-weight-bold">
+
+                        <!-- Grid column -->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="form-contact-phone" class="form-control">
+                                <h3><label for="form-contact-phone" class="text-white">Phone</label></h3>
+                            </div>
+                        </div>
+                        <!-- Grid column -->
+
+                        <!-- Grid column -->
+                        <div class="col-md-6">
+                            <div class="md-form mb-0">
+                                <input type="text" id="form-contact-company" class="form-control">
+                                <h3><label for="form-contact-company" class="text-white">Company</label></h3>
+                            </div>
+                        </div>
+                        <!-- Grid column -->
+
+                    </div>
+                    <!-- Grid row -->
+
+                    <!-- Grid row -->
+                    <div class="row tipografia1 font-weight-bold">
+                        <!-- Grid column -->
+                        <div class="col-md-12">
+                            <div class="md-form mb-0">
+                                <textarea type="text" id="form-contact-message" class="form-control md-textarea" rows="3"></textarea>
+                                <h3><label class="active text-white" for="form-contact-message">Your message</label></h3>
+                                <a class=" boton btn-floating btn-lg blue waves-effect waves-light border-radius text-center"><i class="far fa-paper-plane"></i></a>
+                            </div>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-lg-4 text-center d-flex justify-content-center font-weight-bold animated bounceInRight">
+                <div class="color tipografia2">
+                    <blockquote class="blockquote my-5">
+                        <h2 class=" text-white tipografia2">Contact Information</h2>
+                        <ul class="text-lg-left list-unstyled">
+                            <li>
+                                <h3 class="text-white text-center"><i class="fas fa-map-marker-alt pr-2 text-white"></i>Puntarenas,CR</h3>
+                            </li>
+                            <li>
+                                <h3 class="text-white text-center"><i class="fas fa-phone pr-2 text-white"></i>(+506)8610-7395</h3>
+                            </li>
+                            <li>
+                                <h3 class="text-white text-center"><i class="fas fa-envelope pr-2 text-white"></i>food@gmail.com</h3>
+                            </li>
+                        </ul>
+                        <hr class="hr-light my-4">
+                    </blockquote>
+                </div>
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Form with header -->
+
+</section>
+
+<!-- Section: Contact -->
+
+
+<?php get_footer();?>
---
+++ page-gallery.php
@@ -0,0 +1,273 @@
+<?php get_header();?>
+<div>
+
+    <!-- Seccion Galeria sección superior -->
+    <div class="bannerGaleria mb-5">
+        <div class=" col-md-12 col-lg-12 ">
+            <div class="text-white ">
+                <div class="row aboutBanner align-items-center animated bounceInUp">
+                    <div class="col text-center text-white">
+                        <h1 class="tipografia1 "> <strong class="text-light">Western Food</strong></h1>
+                        <h2 class="tipografia2 mx-3">Our specialty</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Seccion galeria sección superior-->
+
+    <!-- Card deck sección superior -->
+    <div class="card-deck mx-1 animated bounceInLeft delay-1s">
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center overlay">
+                <p class="galeria1 ml-1 img-fluid "></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Millanesa Beef</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$4.99</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria2 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Pork Root</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$5.99</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria3 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Chiken Royce</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$5.50</button>
+            </div>
+        </div>
+        <!-- Card sección superior -->
+
+    </div>
+    <!-- Card deck -->
+
+    <!-- Seccion Galeria sección central -->
+    <div class="bannerGaleria2 mb-5">
+        <div class=" col-md-12 col-lg-12 ">
+            <div class="text-white ">
+                <div class="row aboutBanner align-items-center animated bounceInUp delay-2s">
+                    <div class="col text-right text-white mr-5">
+                        <h1 class="tipografia1  "> <strong class="text-light">Seafood</strong></h1>
+                        <h2 class="tipografia2 mx-3 ">Delights of the sea</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Seccion galeria sección central-->
+
+    <!-- Card deck sección central-->
+    <div class="card-deck mx-1 animated bounceInRight delay-3s">
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria4 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Shrimp Apple</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$6.99</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria5 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Jumbo Sea</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$9.99</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria6 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Royal Soup</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$7.99</button>
+            </div>
+        </div>
+        <!-- Card sección central -->
+
+    </div>
+    <!-- Card deck -->
+
+    <!-- Seccion Galeria sección inferior -->
+    <div class="bannerGaleria3 mb-5">
+        <div class=" col-md-12 col-lg-12 ">
+            <div class="text-white ">
+                <div class="row aboutBanner align-items-center animated bounceInUp delay-4s">
+                    <div class="col text-center text-white mb-10">
+                        <h1 class="tipografia1  "> <strong class="text-light">Desserts</strong></h1>
+                        <h2 class="tipografia2 mx-3 ">Gourmet Desserts</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin Seccion galeria sección inferior-->
+
+    <!-- Card deck sección inferior -->
+    <div class="card-deck mx-1 animated bounceInLeft delay-5s">
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria7 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Sweet King</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$4.99</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria8 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Rollex Shock</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$6.50</button>
+            </div>
+        </div>
+        <!-- Card -->
+
+        <!-- Card -->
+        <div class="card mb-4">
+
+            <!--Card image-->
+            <div class="view zoom d-flex justify-content-center  overlay">
+                <p class="galeria9 ml-1 img-fluid"></p>
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+
+                <!--Title-->
+                <h4 class="card-title text-dark tipografia2 font-weight-bold text-center">Sweet Shadow</h4>
+
+                <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
+                <button class="btn btn-warning text-light mx-2 mt-3 float-right">$1.50</button>
+            </div>
+        </div>
+        <!-- Card sección inferior -->
+    </div>
+</div>
+
+<?php get_footer();?>
